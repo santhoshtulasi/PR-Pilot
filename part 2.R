14af4aa6acee4d6bf62329c461df1fd6833db7f0
santhoshtulasi <santsanthu042001@gmail.com>
2026-03-20 17:52:47 +0530

    Refactor project structure and enhance core functionality with additional features.

---
+++ part 2.R
@@ -0,0 +1,39 @@
+a=c(23,23,34,45,45)
+a
+class(a)
+b =c ("sandy",3)
+b
+class(b)
+c=c(T,F,TRUE,FALSE)
+c
+class(c)
+d <- c(98,67,T)
+d
+class(d)
+e = c("sandy",98,67)
+e
+class(e)
+marks=c(98,87,88,98,99,100)
+print(marks)
+print(marks[5])
+sub=c('kan','maths',"soc",'sci','eng','hin')
+names(marks)=sub
+marks
+print(marks['kan'])
+a=c(23,45,6,78,94)
+mean(a)
+sum(a)
+prod(a)
+mode(a)
+sd(a)
+median(a)
+b=c(2,4,5,67,87,65,NA,67)
+print(b)
+sum(b,na.rm = TRUE)
+e=c(100,200,300,400,500,600)
+f=c('w',"a",'n','t','e','d')
+e[c(1,2)]
+e
+e[2:6]
+e[e>100]
+e>100
